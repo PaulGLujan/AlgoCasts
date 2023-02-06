1d66960b928c1db41fdede753b8ca453e3724573
Paul <paulglujan@gmail.com>
2023-02-06 12:19:10 -0800

    pyraminds and steps

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,39 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, stair = "") {
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  if (n === row) {
+    return;
+  }
+
+  if (stair.length === n * 2 - 1) {
+    console.log(stair);
+    return pyramid(n, row + 1, "");
+  }
+
+  if (stair.length >= midpoint - row && stair.length <= midpoint + row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+
+  pyramid(n, row, stair);
+}
 
 module.exports = pyramid;
+
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let level = "";
+//     for (let column = 0; column < n * 2 - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += "#";
+//       } else {
+//         level += " ";
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,17 +17,57 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let i = 1; i <= n; i++) {
-    let row = "";
-    while (row.length < i) {
-      row += "#";
-    }
-    while (row.length < n) {
-      row += " ";
-    }
-    console.log(row);
+function steps(n, row = 0, stair = "") {
+  if (row === n) {
+    return;
+  }
+
+  if (stair.length === n) {
+    console.log(stair);
+    return steps(n, row + 1, (stair = ""));
+  }
+
+  if (stair.length <= row) {
+    return steps(n, row, stair + "#");
+  } else {
+    return steps(n, row, stair + " ");
   }
 }
 
 module.exports = steps;
+
+// function steps(n, curr = 1) {
+//   if (curr === n) {
+//     console.log("#".repeat(n));
+//   } else {
+//     console.log("#".repeat(curr) + " ".repeat(n - curr));
+//     steps(n, curr + 1);
+//   }
+// }
+
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += "#";
+//       } else {
+//         stair += " ";
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
+
+// function steps(n) {
+//   for (let i = 1; i <= n; i++) {
+//     let row = "";
+//     while (row.length < i) {
+//       row += "#";
+//     }
+//     while (row.length < n) {
+//       row += " ";
+//     }
+//     console.log(row);
+//   }
+// }
