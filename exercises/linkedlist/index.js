58876c2a006fea6de86dc0d086d57c17ed2da7e2
Paul <paulglujan@gmail.com>
2023-02-14 16:25:02 -0800

    Refactor

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,105 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.insertAt(data, 0);
+  }
+
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+
+  getFirst() {
+    return this.getAt(0);
+  }
+
+  getLast() {
+    return this.getAt(this.size() - 1);
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    this.removeAt(0);
+  }
+
+  removeLast() {
+    this.removeAt(this.size() - 1);
+  }
+
+  insertLast(data) {
+    this.insertAt(data, this.size());
+  }
+
+  getAt(index) {
+    if (!this.head) {
+      return null;
+    }
+
+    let node = this.head;
+    for (let i = 0; i < index; i++) {
+      if (!node.next) {
+        return null;
+      }
+      node = node.next;
+    }
+    return node;
+  }
+
+  removeAt(index) {
+    if (!this.head) {
+      return;
+    }
+
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+
+    const previous = this.getAt(index - 1);
+    if (!previous || !previous.next) {
+      return;
+    }
+
+    previous.next = previous.next.next;
+  }
+
+  insertAt(data, index) {
+    if (!this.head) {
+      this.head = new Node(data);
+      return;
+    }
+
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+
+    const previous = this.getAt(index - 1) || this.getLast();
+
+    const node = new Node(data, previous.next);
+    previous.next = node;
+  }
+}
 
 module.exports = { Node, LinkedList };
